550f89ddb11fba10cead64832035905cd4d57078
franciscoplaza <165975148+franciscoplaza@users.noreply.github.com>
2024-04-04 21:42:44 +0000

    registrar asistente listo

--- Evento.h
+++ Evento.h
@@ -11,18 +11,18 @@ class Evento{
         string ubicacion;
         int duracion;
         string tema;
-        vector<Asistente> asistentes;
+        vector<Asistente*> asistentes;
     public:
         Evento(string tipo, string ubicacion, int duracion, string tema);
         string getTipo();
         string getUbicacion();
         int getDuracion();
         string getTema();
-        vector<Asistente> getAsistentes();
+        vector<Asistente*> getAsistentes();
         void setTipo(string tipo);
         void setUbicacion(string ubicacion);
         void setDuracion(int duracion);
         void setTema(string tema);
-        void setAsistente(Asistente asistente);
+        void setAsistente(Asistente* asistente);
         ~Evento();
 };
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include "Evento.h"
+#include "Asistente.h"
 using namespace std;
 
 void mostrarMenu(){
@@ -19,7 +20,7 @@ Evento* crearEvento(){
     cout<<"Ingrese la ubicación del evento: ";
     cin>>ubicacion;
 
-    cout<<"Ingrese la duración del evento: ";
+    cout<<"Ingrese la duración del evento en minutos: ";
     cin>>duracion;
 
     cout<<"Ingrese el tema o género del evento: ";
@@ -29,9 +30,66 @@ Evento* crearEvento(){
     return evento;
 }
 
+Asistente* crearAsistente(){
+    string tipo;
+    string nombre;
+    int edad;
+    string institucion;
+
+    cout<<"Ingrese el tipo de asistente: ";
+    cin>>tipo;
+
+    cout<<"Ingrese el nombre del asistente: ";
+    cin>>nombre;
+
+    cout<<"Ingrese la edad del asistente: ";
+    cin>>edad;
+
+    cout<<"Ingrese la empresa o institución del asistente: ";
+    cin>>institucion;
+
+    Asistente* asistente = new Asistente(tipo, nombre, edad, institucion);
+    return asistente;
+}
+
+void mostrarTiposEventos(vector<Evento*> eventos){
+    vector<string> tipos;
+    for(Evento* evento: eventos){
+        string tipo = evento->getTipo();
+
+        bool encontrado = false;
+        for (int i = 0; i < tipos.size(); i++){
+            if(tipos[i] == tipo){
+                encontrado = true;
+                break;
+            }
+        }
+
+        if (!encontrado){
+            tipos.push_back(tipo);
+        }
+        
+    }
+}
+
+void mostrarEventos(vector<Evento*> eventos){
+    int i = 1;
+    cout<<"Lista de Eventos:"<<endl;
+    for(Evento* evento: eventos){
+        cout<<i++<<".Tipo de evento: "<<evento->getTipo()<<" | Tema: "<<evento->getTema()<<" | Ubicación: "<<evento->getUbicacion()<<endl;
+    }
+}
+
 int main(){
     vector<Evento*> eventos;
     int opcion;
+    Asistente* asistente;
+
+    Evento* e1 = new Evento("Concierto", "Coquimbo", 120, "Rock");
+    Evento* e2 = new Evento("Conferencia", "Copiapo", 30, "Salud");
+
+    eventos.push_back(e1);
+    eventos.push_back(e2);
 
     do{
         mostrarMenu();
@@ -44,8 +102,16 @@ int main(){
                 cout<<"Se ha creado el evento correctamente!"<<endl;
                 break;
             case 2:
-                cout<<"2"<<endl;
+                asistente = crearAsistente();
+                mostrarEventos(eventos);
+                int numeroEscogido;
+                cout<<"Seleccione el número del evento al que desea agregar al asistente: "<<endl;
+                cin>>numeroEscogido;
+
+                eventos[numeroEscogido-1]->setAsistente(asistente);
                 break;
+
+
             case 3:
                 cout<<"3"<<endl;
                 break;
